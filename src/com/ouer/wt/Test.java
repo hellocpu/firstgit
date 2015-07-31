db9d4fce9cdda23d999bed714495f253e0f07440
zhenyuanzi <zhenyuanzi@ixiaopu.com>
2015-07-31 14:46:25 +0800

    first commit

---
+++ src/com/ouer/wt/Test.java
@@ -0,0 +1,10 @@
+package com.ouer.wt;
+/**
+ * @author zhenyuanzi
+ * create date：2015年7月31日 下午2:43:34
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello,world!");
+	}
+}
